c960a77625d28e8d3e9a98b79d6d032c802c3734
Doxramos <admin@guildemporium.net>
2016-01-25 00:01:01 -0500

    Added More To Pulling Users through API functions

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__file__).'/config.php');
+
 if(isset($_GET['query'])) {
-	echo "Return Details";
 	switch($_GET['query']) {
 		case "members":
 		$function = new Accounts();
--- app/views/api.php
+++ app/views/api.php
@@ -1,1 +1,9 @@
-More Coming Soon
+<?php
+$api_member_roster = "https://guildemporium.net/api.php?query=members";
+$file_contents = @file_get_contents($api_member_roster); // omit warnings
+$memberRoster = json_decode($file_contents, true);
+echo "Members Pulled with API";
+foreach($memberRoster as $member) {
+	echo "<br />".$member;
+}
+
