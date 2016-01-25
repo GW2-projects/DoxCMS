be3112ece91223f67f31fd427095f9d1e1f48df4
Doxramos <admin@guildemporium.net>
2016-01-25 03:26:55 -0500

    More Changes to the registration to finalize reg.

--- app/views/api.php
+++ app/views/api.php
@@ -1,9 +1,14 @@
 <?php
-$api_member_roster = "https://guildemporium.net/api.php?query=members";
+$api_member_roster = "https://guildemporium.net/api.php?query=members&apikey=10123";
 $file_contents = @file_get_contents($api_member_roster); // omit warnings
 $memberRoster = json_decode($file_contents, true);
 echo "Members Pulled with API";
+try {
 foreach($memberRoster as $member) {
 	echo "<br />".$member;
+}
+}
+catch(Exception $e) {
+	echo $e->getMessage();
 }
 
